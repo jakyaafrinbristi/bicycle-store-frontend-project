94a611d65310665dfd63b5affea80c3229e254ec
Jakya Afrin Bristi <jakyaafrinb@gmail.com>
2025-05-14 10:40:43 +0600

    last-updated

--- src/pages/Customer/CustomerOverview.tsx
+++ src/pages/Customer/CustomerOverview.tsx
@@ -5,12 +5,8 @@ const CustomerOverview = () => {
 
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-      <div className="bg-green-100 text-green-800 p-4 rounded-xl shadow">
-        <h2 className="text-xl font-bold">Completed Orders</h2>
-        <p className="text-3xl">h1</p>
-      </div>
-     
+    <div >
+      <h1>Customer overview</h1>
       </div>
 
   );
